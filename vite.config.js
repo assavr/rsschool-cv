6fda0ab0af786be830b3c2769ec54ff73460148a
Anastasia Savrukhina <assavr0@gmail.com>
2024-07-02 21:00:30 +0300

    feat: add importmap

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,3 @@
-const isCodeSandbox = !!process.env.SANDBOX_URL;
-
 export default {
-  root: "src/",
-  publicDir: "../static/",
   base: "./",
-  server: {
-    host: true,
-    open: !isCodeSandbox, // Open if it's not a CodeSandbox
-  },
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-    sourcemap: true,
-  },
 };
